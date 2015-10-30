5882d008045bd381b15785f4670490a83db1cdf1
Bojan Radojevic <bradojevic@gmail.com>
2015-10-30 18:36:02 +0100

    Release 0.1

--- README.rst
+++ README.rst
@@ -1,2 +1,23 @@
 # django-action-log
-Simple and small action logger for django. You can log arbitrary action with username and optional payload that goes with your action. 
+Simple and small action logger for django. You can log arbitrary action with username and optional payload that goes with your action.
+
+
+django-action-log
+-----------------
+
+Simple and small action logger for django. You can log arbitrary action with username and optional payload that goes with your action.
+
+`django-action-log` is a simple, small, reusable application that provides
+support for logging custom actions in django.
+
+You can log arbitrary action with username and optional payload that goes with your action.
+
+Example
+-------
+from action_log.models import ActionRecord
+
+ActionRecord.objects.create_record(
+    'example_action_name',
+    username='username or some identifier',
+    payload='{"param1": "custom value", "param2": "different value"}'
+)
--- setup.py
+++ setup.py
@@ -12,9 +12,11 @@ os.chdir(
     os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir))
 )
 
+VERSION = '0.1'
+
 setup(
     name='django-action-log',
-    version='0.1',
+    version=VERSION,
     packages=[
         'action_log',
         'action_log/migrations',
@@ -24,16 +26,16 @@ setup(
     description='Simple and small action logger for django.',
     long_description=README,
     url='https://github.com/bradojevic/django-action-log',
-    download_url='https://github.com/bradojevic/django-action-log/tarball/0.1',
+    download_url='https://github.com/bradojevic/django-action-log/tarball/%s' % VERSION,
     author='Bojan Radojevic',
     author_email='bradojevic@gmail.com',
     classifiers=[
+        'Development Status :: 3 - Alpha'
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
