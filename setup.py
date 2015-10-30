c6e2fb978651679a6d373766c2db3bb7ceb5861c
Bojan Radojevic <bradojevic@gmail.com>
2015-10-30 16:49:07 +0100

    Add setup information.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name='django-action-log',
+    packages=['django-action-log'],
+    version='0.1',
+    description='Simple and small action logger for django.',
+    long_description=open('README.md').read(),
+    author='Bojan Radojevic',
+    author_email='bradojevic@gmail.com',
+    url='https://github.com/bradojevic/django-action-log',
+    download_url='https://github.com/bradojevic/django-action-log/tarball/0.1',
+    keywords=[
+        'django',
+        'logging',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
