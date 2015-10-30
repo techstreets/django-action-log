1b0ee45ca05d5c8cda62f29f9f514ac5eda641fa
Bojan Radojevic <bradojevic@gmail.com>
2015-10-30 18:20:10 +0100

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,41 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+README_PATH = open(
+    os.path.join(os.path.dirname(__file__), 'README.rst')
+)
+with README_PATH as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(
+    os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir))
+)
+
 setup(
     name='django-action-log',
-    packages=['django-action-log'],
     version='0.1',
+    packages=[
+        'action_log',
+        'action_log/migrations',
+    ],
+    include_package_data=True,
+    license='MIT License',
     description='Simple and small action logger for django.',
-    long_description=open('README.md').read(),
-    author='Bojan Radojevic',
-    author_email='bradojevic@gmail.com',
+    long_description=README,
     url='https://github.com/bradojevic/django-action-log',
     download_url='https://github.com/bradojevic/django-action-log/tarball/0.1',
-    dependency_links=['https://github.com/bradojevic/django-action-log/tarball/0.1'],
-    keywords=[
-        'django',
-        'logging',
-    ],
+    author='Bojan Radojevic',
+    author_email='bradojevic@gmail.com',
     classifiers=[
         'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Framework :: Django',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 )
